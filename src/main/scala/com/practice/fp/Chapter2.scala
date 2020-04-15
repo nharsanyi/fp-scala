991b66836a846722d0d61ed4577a351df6c02694
nikike <nikike@nikoletts-mbp.home>
2020-04-15 19:44:33 +0100

    Add polymorfic example

--- src/main/scala/com/practice/fp/Chapter2.scala
+++ src/main/scala/com/practice/fp/Chapter2.scala
@@ -1,12 +1,17 @@
 package com.practice.fp
 
+import scala.annotation.tailrec
+
+/**
+  * Getting started with functional programming in Scala
+  */
 object Chapter2 {
 
 
   def factorial(n: Int): Int = {
     def go(n: Int, acc: Int): Int = {
       if (n <= 1) acc
-      else go(n - 1, acc * n)
+      else go(n - 1, acc * n) // tail position -> calls it but does nothing with the result
     }
 
     go(n, 1)
@@ -21,13 +26,32 @@ object Chapter2 {
     go(n, 0, 1)
   }
 
+  private def formatResult(name: String, n: Int, f: Int => Int) = {
+    "The %s of %d is %d".format(name, n, f(n))
+  }
+
+  /**
+    * polymorphic function to find the first element of an array that matches the given condition
+    * @param arr array
+    * @param p condition to match
+    * @tparam A
+    * @return index of the element, returns -1 if no element found
+    */
+  def findFirst[A](arr: Array[A], p: A => Boolean): Int = {
+
+    @tailrec
+    def go(pos: Int): Int = {
+      if (pos >= arr.length) -1
+      else if (p(arr(pos))) pos
+      else go(pos + 1)
+    }
+    go(0)
+  }
+
   def main(args: Array[String]): Unit = {
-    println("5 factorial = %d".format(factorial(5)))
-    println("1 factorial = %d".format(factorial(1)))
-    println("3 factorial = %d".format(factorial(3)))
-
-    println("3rd fibonacci = %d".format(fibo(3)))
-    println("1st fibonacci = %d".format(fibo(1)))
-
+    println(formatResult("factorial", 5, factorial))
+    println(formatResult("fibonacci", 5, fibo))
+    println("First index of an even number is %d".format(findFirst[Int](Array(1, 3, 5, 2), x => x % 2 == 0)))
+    println("First index of an uppercase string is %d".format(findFirst[String](Array("Apple", "Banana", "LEMON", "peaR"), x => x.matches("[A-Z]+"))))
   }
 }
