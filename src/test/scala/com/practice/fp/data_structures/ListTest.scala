d3113b1f7e4dfdb88e7eb3f319443d89a5b16c41
nikike <nikike@nikoletts-mbp.home>
2020-04-17 19:00:38 +0100

    Add pattern matching test

--- src/test/scala/com/practice/fp/data_structures/ListTest.scala
+++ src/test/scala/com/practice/fp/data_structures/ListTest.scala
@@ -37,4 +37,15 @@ class ListTest extends FunSuite {
     assert(oneElementList.toString == "1.3, Nil")
   }
 
+  test("pattern matching test") {
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + List.sum(t)
+      case _ => 101
+    }
+    assert(x == 3)
+  }
+
 }
