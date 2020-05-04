2a4b5c4e1ea4a28f5569899fd5277e3d826538e9
nikike <nikike@nikoletts-mbp.home>
2020-05-04 20:44:13 +0100

    Add lift and map2

--- src/main/scala/com/practice/fp/error_handling/HandlingErrors.scala
+++ src/main/scala/com/practice/fp/error_handling/HandlingErrors.scala
@@ -25,9 +25,24 @@ object HandlingErrors {
       case Some(a) if (f(a)) =>  this
       case _ => None
     }
+
+    def lift[A, B](f: A => B): Option[A] => Option[B] = _ map f
+
+    def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+      a.flatMap(aa => b map (bb => f(aa, bb)))
+    }
   }
 
   case class Some[+A](get: A) extends Option[A]
   case object None extends Option[Nothing]
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    if (xs.isEmpty) None
+    else {
+      val mean = xs.sum / xs.size
+      val r = xs.map(x => math.pow(x - mean, 2))
+      Some(r.sum / r.size)
+    }
+  }
 
 }
