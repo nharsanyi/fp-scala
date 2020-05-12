ba2cd0c3e357d505fb149dd4de1020bfe66c2735
nikike <nikike@nikoletts-mbp.home>
2020-05-12 18:52:39 +0100

    Handling Errors

--- src/main/scala/com/practice/fp/error_handling/HandlingErrors.scala
+++ src/main/scala/com/practice/fp/error_handling/HandlingErrors.scala
@@ -8,6 +8,35 @@ object HandlingErrors {
     catch { case _: Exception => None}
   }
 
+  sealed trait Either[+E, +A] {
+    def map[B](f: A => B): Either[E, B] = this match {
+      case Right(a) => Right(f(a))
+      case Left(e) => Left(e)
+    }
+
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = {
+      this match {
+        case Right(a) => f(a)
+        case Left(e) => Left(e)
+      }
+    }
+
+    def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+      case Right(a) => Right(a)
+      case Left(_) => b
+    }
+
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+      for {
+        a <- this
+        bb <- b
+      } yield f(a, bb)
+    }
+
+  }
+  case class Left[+E](value: E) extends Either[E, Nothing]
+  case class Right[+A](value: A) extends Either[Nothing, A]
+
   sealed trait Option[+A] {
 
     def map[B](f: A => B): Option[B] = this match {
